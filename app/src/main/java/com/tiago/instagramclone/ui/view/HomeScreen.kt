8c9595e4fd4972c24d543c31d8696260bd7ce8fe
Tiago <tiagonishigasako@gmail.com>
2023-09-26 03:31:11 -0300

    project Clone Instagram (ToolBar)

---
+++ app/src/main/java/com/tiago/instagramclone/ui/view/HomeScreen.kt
@@ -0,0 +1,28 @@
+package com.tiago.instagramclone.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun HomeScreen() {
+
+
+    Column (modifier = Modifier.fillMaxSize()){
+
+        InstagramToolBar()
+
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+
+fun HomeScreenPreview(){
+    HomeScreen()
+}
